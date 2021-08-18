b8c850f044892566f682be9ad957b4611f73d2ff
jishamol <jishamoltt@gmail.com>
2021-08-18 09:31:55 -0400

    Added new class to print student provided information from user input.

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -2,14 +2,11 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class HelloWorld {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Word");
-        Scanner scan= new Scanner(System.in);
-        System.out.println("Please enter ur name");
-
 
     }
 }

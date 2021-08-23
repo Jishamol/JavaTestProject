8aea04726b79750b04b3fdfef1227aaf880c75c4
jishamol <jishamoltt@gmail.com>
2021-08-23 09:56:07 -0400

    Added new class If else condition Addition subtraction

---
+++ src/com/company/AdditionSubtraction.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+    public class AdditionSubtraction {
+        public static void main(String args[])
+        {
+             int FirstNumber,SecondNumber,addNum,subNum;
+             Scanner scan=new Scanner(System.in);
+             System.out.println("Enter the first number");
+             FirstNumber=scan.nextInt();
+             System.out.println("Enter the second number");
+             SecondNumber= scan.nextInt();
+             addNum=FirstNumber+SecondNumber;
+
+                if (FirstNumber>=SecondNumber)
+                 {
+                      subNum=FirstNumber-SecondNumber;
+                 }
+                 else
+                 {
+                        subNum=SecondNumber-FirstNumber;
+                 }
+
+             System.out.println("Sum of two numbers is: "+addNum);
+             System.out.println("Difference between two numbers is: "+subNum);
+        }
+    }

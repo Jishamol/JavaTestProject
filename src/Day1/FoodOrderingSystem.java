22a7b83b9f632b60dd4e9abe6a93b21e80669924
jishamol <jishamoltt@gmail.com>
2021-08-26 09:48:20 -0400

    Added new class for food ordering

--- src/Day1/FoodOrderingSystem.java
+++ src/Day1/FoodOrderingSystem.java
@@ -1,4 +1,4 @@
-package com.company;
+package Day1;
 
 import java.awt.desktop.SystemEventListener;
 import java.util.Scanner;
---
+++ src/Day1/studentDetails.java
@@ -0,0 +1,21 @@
+package Day1;
+
+import java.util.Scanner;
+
+    public class studentDetails
+    {
+
+         public static void main(String args[])
+         {
+            int sAge;
+            String stname;
+            Scanner scan=new Scanner(System.in);
+            System.out.println("Enter Student Name");
+            stname=scan.next();
+            System.out.println("Enter Student Age");
+            sAge= scan.nextInt();
+            System.out.println("Student details are:");
+            System.out.println("Name is :"+stname);
+            System.out.println("Age is :"+sAge);
+        }
+    }
